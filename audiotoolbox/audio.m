525463e7079ab24d839f97948340f3b32d543408
Jaana Burcu Dogan <jbd@google.com>
2016-07-28 13:29:55 -0700

    implement reading

--- audiotoolbox/audio.m
+++ audiotoolbox/audio.m
@@ -35,8 +35,32 @@ int audio_seek(ExtAudioFileRef file, int sampleIndex) {
   return 0;
 }
 
-int audio_read(ExtAudioFileRef file, int size, const float* dst) {
+int audio_read(ExtAudioFileRef file, int size, uint numChannels, const float* dst) {
+  OSStatus err;
 
+  uint totalFrames = size/numChannels;
+  uint framesToRead = totalFrames;
+
+  uint numReadFrames = 0; // numReadFrames could be less than totalFrames.
+
+  while (framesToRead < totalFrames) {
+    AudioBufferList bufList;
+    bufList.mNumberBuffers = 1;
+    bufList.mBuffers[0].mNumberChannels = numChannels;
+    bufList.mBuffers[0].mDataByteSize = framesToRead * numChannels * sizeof(float);
+    bufList.mBuffers[0].mData = (void*)(&dst[framesToRead * numChannels]);
+
+    UInt32 numFrames;
+    err = ExtAudioFileRead(file, &numFrames, &bufList);
+    if (err != noErr) {
+      return err;
+    }
+    if (numFrames == 0) {
+      break;
+    }
+    framesToRead = totalFrames - numFrames;
+    numReadFrames += numFrames;
+  }
   return 0;
 }
 
